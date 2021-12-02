1511d10857015b15b3b88280ece6709b2f60e318
Felipe <felipesubi@hotmail.com>
2021-12-02 16:43:36 -0300

    usando massa de dados

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,9 +1,12 @@
 /// <reference types="cypress"/> 
-
+const perfil = require('../fixtures/perfil.json')
 context ('Funcionalidade Login',() =>{
-
+    beforeEach(() => {
+        cy.visit('minha-conta/')
+    });    
+    
     it (' Deve fazer login com sucesso',() =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+       
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -11,8 +14,24 @@ context ('Funcionalidade Login',() =>{
         cy.get('.page-title').should('contain','Minha conta')
         cy.get('.woocommerce-MyAccount-content').should('contain','Olá')
     })
+
+    it('Deve fazer login com sucesso - usando arquivo de dados',() =>{
+        cy.get('#username').type(perfil.Usuario)
+        cy.get('#password').type(perfil.Senha)
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.page-title').should('contain','Minha conta')
+        cy.get('.woocommerce-MyAccount-content').should('contain','Olá')
+    })
+    it.only('Deve fazer login com sucesso - usando fixture',() =>{
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.Usuario)
+            cy.get('#password').type(dados.Senha, {log: false})
+            cy.get('.woocommerce-form > .button').click()
+    })    
+    })
     it('Deve exibir uma tratativa de erro em caso de preenchimento usuario invalidos', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+       
         cy.get('#username').type('aluo_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -21,7 +40,7 @@ context ('Funcionalidade Login',() =>{
 
     })
     it('Deve exibir uma tratativa de erro em caso de preenchimento senha invalidos', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+       
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
@@ -29,4 +48,5 @@ context ('Funcionalidade Login',() =>{
         cy.get('.woocommerce-error').should('contain', 'A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
 
     })
+    
 })
