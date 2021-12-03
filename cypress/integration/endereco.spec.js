dc8a2eff8e138b675e2bf969d8e04e57241acc7a
Felipe <felipesubi@hotmail.com>
2021-12-03 12:24:43 -0300

    criando cenarios com page objects

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,5 +1,5 @@
 /// <reference types ="cypress"/>
-
+import EnderecoPage from '../support/page-objects/endereco.page'
 describe('Funcionalidade endereços - faturamento e entrega', () => {
     beforeEach(() => {
         cy.visit('minha-conta/')
@@ -8,7 +8,8 @@ describe('Funcionalidade endereços - faturamento e entrega', () => {
         })
         
     });
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
+    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Joao','jacinto','youtube','Av santos dummond','25552','Santos','Sao Paulo','01452111','11777774444','joaojacinto@dominio.com')
+        cy.get('.woocommerce-message').should('contain','Endereço alterado com sucesso.')
     });
 });
